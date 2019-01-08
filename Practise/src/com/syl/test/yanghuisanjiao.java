043a46c86b2d02f1bd4a5b4c06b2b3ee5c613600
sun <269404001@qq.com>
2019-01-08 22:11:07 +0800

    新增练习 杨辉三角

---
+++ Practise/src/com/syl/test/yanghuisanjiao.java
@@ -0,0 +1,40 @@
+package com.syl.test;
+
+/*
+ * method ´òÓ¡Ñî»ÔÈý½ÇÐÎ
+ * 
+ * 1
+ * 1	1
+ * 1	2	1
+ * 1	3	3	1
+ * 1	4	6	4	1
+ * 1	5	10	10	5	1
+ * 1	6	15	20	15	6	1
+ * 1	7	21	35	35	21	7	1 
+ * 
+ * @author arya
+ *
+ */
+public class yanghuisanjiao {
+	public static void main(String[] args) {
+		int n = 10;
+		getYHSJ(n);
+	}
+
+	public static void getYHSJ(int n) {
+		int[][] arr = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				if (j == 0 || j == i) {
+					arr[i][j] = 1;
+				} else {
+					arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+				}
+				System.out.print(arr[i][j] + "\t");
+				if (j == i) {
+					System.out.println();
+				}
+			}
+		}
+	}
+}
