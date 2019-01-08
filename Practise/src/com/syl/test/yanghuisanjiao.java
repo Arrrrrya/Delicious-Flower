cbfea129870bad438041cac4163b75fc9588ffe0
sun <269404001@qq.com>
2019-01-08 22:25:53 +0800

    优化array容量大小、优化输入

--- Practise/src/com/syl/test/yanghuisanjiao.java
+++ Practise/src/com/syl/test/yanghuisanjiao.java
@@ -22,8 +22,9 @@ public class yanghuisanjiao {
 	}
 
 	public static void getYHSJ(int n) {
-		int[][] arr = new int[n][n];
+		int[][] arr = new int[n][];
 		for (int i = 0; i < n; i++) {
+			arr[i] = new int[i + 1];
 			for (int j = 0; j <= i; j++) {
 				if (j == 0 || j == i) {
 					arr[i][j] = 1;
@@ -31,10 +32,8 @@ public class yanghuisanjiao {
 					arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
 				}
 				System.out.print(arr[i][j] + "\t");
-				if (j == i) {
-					System.out.println();
-				}
 			}
+			System.out.println();
 		}
 	}
 }
