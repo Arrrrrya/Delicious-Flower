fdd78089302e919534fec16d117804a1c6b5efb1
sun <269404001@qq.com>
2019-01-10 20:52:17 +0800

    新增中文类名测试

---
+++ Practise/src/com/syl/test/中文测试.java
@@ -0,0 +1,13 @@
+package com.syl.test;
+
+public class 中文测试 {
+
+	public void 输入中文() {
+		System.out.println("第一次用中文类名！");
+	}
+
+	public static void main(String[] args) {
+		中文测试 中文测试 = new 中文测试();
+		中文测试.输入中文();
+	}
+}
