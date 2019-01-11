03069840b93616851dc4640f4320bb204ec59ea0
sun <269404001@qq.com>
2019-01-11 18:32:49 +0800

    add 004 and 005

---
+++ Practise/src/com/syl/test/_004_ChengFaBiao.java
@@ -0,0 +1,25 @@
+package com.syl.test;
+
+/**
+ * .打印输入九九乘法表
+ * 
+ * @author syl
+ * @syl the bug is not what you see,but what it is.
+ */
+public class _004_ChengFaBiao {
+	public void getChengFaBiao(int n) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i + "x" + j + "=" + i * j + "\t");
+				if (j == i) {
+					System.out.println();
+				}
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+		new _004_ChengFaBiao().getChengFaBiao(9);
+	}
+}
---
+++ Practise/src/com/syl/test/_005_DengYaoSJ.java
@@ -0,0 +1,30 @@
+package com.syl.test;
+
+/*
+ * .打印输入等腰三角形,输入4,则输出：
+ *            *
+ *           ***
+ *          *****
+ *         *******
+ *   
+ * @author syl
+ * @syl the bug is not what you see,but what it is.
+ */
+public class _005_DengYaoSJ {
+	public void getDengYaoSJ(int n) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= (n - i); j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= (2 * i - 1); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		new _005_DengYaoSJ().getDengYaoSJ(5);
+	}
+}
