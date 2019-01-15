b7786f448be70f34846f4dbb76f30fe919f85143
sun <269404001@qq.com>
2019-01-15 20:14:01 +0800

    update 012

---
+++ Practise/src/com/syl/test/_012_ClazzDemo.java
@@ -0,0 +1,51 @@
+package com.syl.test;
+
+/**
+ * 
+ * @question
+ * @link
+ *
+ * @author syl(sun_mir@qq.com)
+ * @syl the bug is not what you see,but what it is.
+ */
+public class _012_ClazzDemo {
+	public static void main(String[] args) {
+		new People("张三").run();
+
+		new People("张三").new Student("李四").study();
+
+		new People("张三").new Student("李四").run();
+	}
+}
+
+class People {
+	private String name;
+
+	public People(String name) {
+		this.name = name;
+	}
+
+	public void run() {
+		System.out.println(name + " is a people");
+	}
+
+	public void study() {
+		new Student("").study();
+	}
+
+	class Student {
+		private String name;
+
+		public Student(String name) {
+			this.name = name;
+		}
+
+		public void study() {
+			System.out.println(name + " is a student");
+		}
+
+		public void run() {
+			new People("").run();
+		}
+	}
+}
