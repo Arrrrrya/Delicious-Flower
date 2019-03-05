25450f0601c185b386a0c7e572efad231ce5c6b5
sun <269404001@qq.com>
2019-03-05 20:06:11 +0800

    update

--- Practise/src/com/syl/test/_983_FileDemo.java
+++ Practise/src/com/syl/test/_983_FileDemo.java
@@ -1,6 +1,8 @@
 package com.syl.test;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -13,39 +15,54 @@ import java.util.Arrays;
  * @syl the bug is not what you see,but what it is.
  */
 public class _983_FileDemo {
-	public String[] getFileName(String path) {
-		File file = new File(path);
-		String[] fileName = file.list();
-		return fileName;
-
-	}
-
 	public void getAllFileName(String path, ArrayList<String> fileName) {
 		File file = new File(path);
-		File[] files = file.listFiles();
-		String[] names = file.list();
-		if (names != null)
-			fileName.addAll(Arrays.asList(names));
-		for (File a : files) {
-			if (a.isDirectory()) {
-				getAllFileName(a.getAbsolutePath(), fileName);
-			}
-
+		if (!file.exists()) {
+			System.out.println("wrong path !");
+			return;
 		}
 
+		File[] files = file.listFiles();
+		for (File f : files) {
+			fileName.add(f.getAbsolutePath());
+			if (f.isDirectory()) {
+				getAllFileName(f.getAbsolutePath(), fileName);
+			}
+		}
+	}
+
+	void writeToFile(String resultFile, ArrayList<String> fileList) {
+		try {
+			File file = new File(resultFile);
+			if (file.exists()) {
+				file.delete();
+			}
+			FileWriter out = new FileWriter(file, true);
+			String data = "";
+			for (int i = 0; i < fileList.size(); i++) {
+				data = fileList.get(i);
+				out.write(data + "\r\n");
+			}
+			out.close();
+			System.out.println("\nall fileNames are written into " + file.getAbsolutePath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
-//		String filePath = "x:\\±ÚÖ½";
-//		String filePath = "e:\\±ÚÖ½\\Î¢ÐÅ½ØÍ¼_20181221202228.png";
-		String filePath = "e:\\±ÚÖ½";
+		String filePath = "E:\\Z_test";
 		ArrayList<String> fileList = new ArrayList<String>();
 		new _983_FileDemo().getAllFileName(filePath, fileList);
-		if (null != fileList) {
-			for (int i = 0; i < fileList.size(); i++) {
-				System.out.println(fileList.get(i));
-			}
+
+		for (int i = 0; i < fileList.size(); i++) {
+			System.out.println(fileList.get(i));
 		}
+
+		String resultFile = "D:\\z.txt";
+		new _983_FileDemo().writeToFile(resultFile, fileList);
+
 	}
 
 }
