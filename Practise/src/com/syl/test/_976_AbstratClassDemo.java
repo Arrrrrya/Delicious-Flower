bafd1c7dd2f02e818ba47d505a92cce582510007
sunyl <sunyl@P020162J19W10J1.BJ.bhh.com.cn>
2019-05-15 16:53:40 +0900

    add abstract test

---
+++ Practise/src/com/syl/test/_976_AbstratClassDemo.java
@@ -0,0 +1,22 @@
+package com.syl.test;
+
+public abstract class _976_AbstratClassDemo {
+
+	private String name;
+	
+	public _976_AbstratClassDemo(String name){
+		System.out.println("I am Abstract");
+		this.setName(name);
+		System.out.println(name);
+	}
+	
+	public abstract void run();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
