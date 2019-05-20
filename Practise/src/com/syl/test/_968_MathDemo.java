a4ed1c7c176aa38ef69d0e5d438b05bc3471f84f
sunyl <sunyl@P020162J19W10J1.BJ.bhh.com.cn>
2019-05-20 17:58:17 +0900

    add MathDemo

---
+++ Practise/src/com/syl/test/_968_MathDemo.java
@@ -0,0 +1,18 @@
+package com.syl.test;
+
+/**
+ * 输入一行字符，分别统计出其英文字母、空格、数字和其它字符的个数。
+ * 
+ * @author sunyl
+ *
+ */
+public class _968_MathDemo {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+
+}
