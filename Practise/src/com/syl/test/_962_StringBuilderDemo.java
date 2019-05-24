3f10e1435799d4e31195e691c7987fa67044ab36
sunyl <sunyl@P020162J19W10J1.BJ.bhh.com.cn>
2019-05-24 10:37:59 +0900

    add StringBuilderDemo

---
+++ Practise/src/com/syl/test/_962_StringBuilderDemo.java
@@ -0,0 +1,38 @@
+package com.syl.test;
+
+public class _962_StringBuilderDemo {
+
+	public static void main(String[] args) {
+		long startCreatString = System.currentTimeMillis();
+		String string = "";
+		for (int i = 0; i < 1000000; i++) {
+			string += 1;
+		}
+		long endCreatString = System.currentTimeMillis();
+		System.out.println("string length = " + string.length() + "\r\ncreate string cost time: "
+				+ (endCreatString - startCreatString) + " ms");
+		// 697404 ms ≈ 11 min
+		
+		long startCreatStringBuffer = System.currentTimeMillis();
+		StringBuffer stringBuffer = new StringBuffer();
+		for (int i = 0; i < 1000000; i++) {
+			stringBuffer.append(1);
+		}
+		long endCreatStringBuffer = System.currentTimeMillis();
+		System.out.println("stringBuffer length = " + stringBuffer.length() + "\r\ncreate stringBuffer cost time: "
+				+ (endCreatStringBuffer - startCreatStringBuffer) + " ms");
+		// 12 ms
+		
+		long startCreatStringBuilder = System.currentTimeMillis();
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < 1000000; i++) {
+			stringBuilder.append(1);
+		}
+		long endCreatStringBuilder = System.currentTimeMillis();
+		System.out.println("stringBuilder length = " + stringBuilder.length() + "\r\ncreate stringBuilder cost time: "
+				+ (endCreatStringBuilder - startCreatStringBuilder) + " ms");
+		// 8 ms
+		
+	}
+
+}
