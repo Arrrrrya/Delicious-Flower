2a2aff1705705679037bae5b0ad183adb0450af5
sunyl <sunyl@P020162J19W10J1.BJ.bhh.com.cn>
2019-07-17 20:16:22 +0900

    update

--- Practise/src/com/syl/util/DivisionUtil.java
+++ Practise/src/com/syl/util/DivisionUtil.java
@@ -3,40 +3,50 @@ package com.syl.util;
 public class DivisionUtil {
 
 	public static void main(String[] args) {
-		// 结果保留3位小数(四舍五入)
-		System.out.println(doDivision(2, 3, 3));// 0.667
+		// 除法四舍五入(分子,分母,保留小数位数)
+		System.out.println(doDivision(-2, -3, 3));// 0.667
 		System.out.println(doDivision(20, 2, 3));// 10.000
-		System.out.println(doDivision(5, 8, 1));// 0.13
-
+		System.out.println(doDivision(5, 8, 0));// 1
 	}
 
 	static String doDivision(int fenZi, int fenMu, int remainNumber) {
+		if (remainNumber < 0) {
+			System.err.println("ERROR: the 3rd param [" + remainNumber + "] is invalid !");
+			return "";
+		}
+
 		String result = Double.parseDouble(fenZi + "") / Double.parseDouble(fenMu + "") + "";
 		String[] resultStrings = (result).split("\\.");
 		String beforeDot = resultStrings[0];
-		
+
 		if (resultStrings[1].length() <= remainNumber) {
 			for (int i = 0; i < (remainNumber - resultStrings[1].length()); i++) {
 				result += "0";
 			}
 			return result;
 		}
-		
 
-		
-		String afterDot = resultStrings[1].substring(0, remainNumber);
+		String afterDot = "";
+		if (remainNumber > 0) {
+			afterDot = resultStrings[1].substring(0, remainNumber);
+		}
+
 		char flagNumber = resultStrings[1].charAt(remainNumber);
-		
-		if(remainNumber != 0){
-			if (Double.parseDouble(flagNumber + "") > 4) {
+
+		if (Double.parseDouble(flagNumber + "") > 4) {
+			if (remainNumber > 0) {
 				result = beforeDot + "." + (Integer.parseInt(afterDot) + 1);
 			} else {
-				result = beforeDot + "." + afterDot;
+				result = (Integer.parseInt(beforeDot) + 1) + "";
 			}
-		}else {
-			
+		} else {
+			if (remainNumber > 0) {
+				result = beforeDot + "." + afterDot;
+			} else {
+				result = beforeDot;
+			}
 		}
-		
+
 		return result;
 	}
 
