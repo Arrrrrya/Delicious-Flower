3b2b86552caec756ac5c4b9d3078342a96de4345
sunyl <sunyl@P020162J19W10J1.BJ.bhh.com.cn>
2019-07-25 10:28:57 +0900

    UPDATE

--- Practise/src/com/syl/test/_960_ParamTest.java
+++ Practise/src/com/syl/test/_960_ParamTest.java
@@ -5,6 +5,7 @@ public class _960_ParamTest {
 	public static void main(String[] args) {
 		System.out.println(returnMaxNumber(0, 1, 7));
 		System.out.println(returnMaxNumber(101, 3, 4, 6, 100));
+		System.out.println(returnMaxNumber2(101, 3, 4, 6, 103));
 
 		System.out.println(returnSearchStringFirstIndex("a", "b", "b", "b", "b", "a", "b", "b"));
 
@@ -30,6 +31,16 @@ public class _960_ParamTest {
 			}
 		}
 		return index;
+	}
+
+	static int returnMaxNumber2(int... a) {
+		int max = a[0];
+		for (int i = 0; i < a.length; i++) {
+			if (max < a[i]) {
+				max = a[i];
+			}
+		}
+		return max;
 	}
 
 }
